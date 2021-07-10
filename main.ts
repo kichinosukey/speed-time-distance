f1f0998640d668fb234f79494af243eb4cf71923
kichinosukey <kichinosuke.fukuhara@gmail.com>
2021-07-10 23:24:44 +0900

    更新 main.ts

--- main.ts
+++ main.ts
@@ -4,7 +4,7 @@ namespace Car{
     //% block="はしる %time %speed"
     export function はしる (time: number, speed: number): void
     {
-        if (input.runningTime() / 1000 == time) {
+        if (input.runningTime() / 1000 <= time) {
             maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, speed)
         }
         else {
